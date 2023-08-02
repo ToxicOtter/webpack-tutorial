6f1d967d20552ccfc93525f0ca481d900a01908d
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-08-01 23:42:41 -0300

    Implementa o loader de sass e substitui o arquivo css

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
             }
         ]
     }
